2ff06cb1a19fb6f816116a3d60a30d29d744d0b6
Sabarish Rajamohan <sabarishrajamohan@gmail.com>
2020-05-12 14:06:59 +0530

    Updates

---
+++ precache-manifest.5b111471ae381c952357faaaa3b9c41d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f78ea64fef8e62ab9f7d5d8cf675a7",
+    "url": "/covid-tracker-app/index.html"
+  },
+  {
+    "revision": "0cd69e17f3d81244b9a8",
+    "url": "/covid-tracker-app/static/css/main.9cdfcb20.chunk.css"
+  },
+  {
+    "revision": "795cac55b55e556d152c",
+    "url": "/covid-tracker-app/static/js/2.19a5b145.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker-app/static/js/2.19a5b145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd69e17f3d81244b9a8",
+    "url": "/covid-tracker-app/static/js/main.0213c812.chunk.js"
+  },
+  {
+    "revision": "f8f2656185712fbb9431",
+    "url": "/covid-tracker-app/static/js/runtime-main.cde3ba1b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker-app/static/media/image.d7265326.png"
+  }
+]);
